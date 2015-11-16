053567dd7306fe653290f5d6e6bc93b89f48505a
tsub <nor.known.to.life.51122@gmail.com>
2015-11-16 15:45:40 +0900

    refactor

--- lib/wifi_geo_location.rb
+++ lib/wifi_geo_location.rb
@@ -1,24 +1,26 @@
 require 'faraday'
 require 'json'
 
-require 'wifi_geo_location/wifi_detail'
+require 'wifi_geo_location/wifi_tower'
 require 'wifi_geo_location/client'
 require 'wifi_geo_location/version'
 
 module WifiGeoLocation
   def self.find(key: nil)
-    wifi_details = WifiGeoLocation::WifiDetail.scan
-
-    request_params = { wifiAccessPoints: [] }
-    wifi_details.each do |detail|
-      request_params[:wifiAccessPoints] << {
-        macAddress: detail[:mac_address],
-        signalStrength: detail[:signal_strength],
-        channel: detail[:channel]
-      }
-    end
-
     client = WifiGeoLocation::Client.new(key: key)
+
+    wifi_towers = WifiGeoLocation::WifiTower.scan
+
+    request_params = {
+      wifiAccessPoints: wifi_towers.map do |tower|
+        {
+          macAddress: tower[:mac_address],
+          signalStrength: tower[:signal_strength],
+          channel: tower[:channel]
+        }
+      end
+    }
+
     response = client.post(request_params)
   end
 end
